f9e4b48f9e6fa0836c2b263314b43ececcbfe89b
srpeddolla <shashidhar.peddolla@outlook.com>
2024-06-30 16:30:31 +0530

    commit

--- terraform_threetierarch/destroy-ecrs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export AWS_PAGER=""
-aws ecr delete-repository \
-    --repository-name ha-app-application-tier \
-    --force
-
-aws ecr delete-repository \
-    --repository-name ha-app-presentation-tier \
-    --force
